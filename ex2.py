2ef8463c04af3691a0aeb063fbebf66931cb8762
yuliqing <yuliqing16@126.com>
2017-12-19 00:15:46 +0800

    mac push test

--- ex2.py
+++ ex2.py
@@ -5,4 +5,4 @@ print("I could have code like this.") # and the coment after is ignored
 # You can also use a comment to "disable" or comment out a piece of code:
 # print "This won't run."
 
-print("this will run")
+print("this will run222")
